34ff305aa54060b54c407721034da367a3daf653
liushuxin <shuxinliu2016@gmail.com>
2022-07-21 13:19:47 +0800

    add calculator

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
+import Calculator from "./calculator/index";
 
 export default function Home() {
   const [state, setState] = useState([]);
@@ -43,36 +44,7 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">SHUXINLIU </a>
         </h1>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-        <label>输入key</label>
+        {/* <label>输入key</label>
         <input
           onChange={(e) => {
             setInputKeyState(e.target.value);
@@ -104,9 +76,9 @@ export default function Home() {
               </div>
             );
           })}
-        </div>
+        </div> */}
       </main>
-
+      <Calculator />
       <footer className={styles.footer}></footer>
     </div>
   );
